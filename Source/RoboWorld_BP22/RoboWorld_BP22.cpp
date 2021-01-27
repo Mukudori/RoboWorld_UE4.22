388a6c4f0df2e6f7d716abca09e81f3e0fe6615a
Влад Шпаков <Vladislav>
2021-01-27 14:42:13 +0700

    Generate CPP files

---
+++ Source/RoboWorld_BP22/RoboWorld_BP22.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoboWorld_BP22.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoboWorld_BP22, "RoboWorld_BP22" );
